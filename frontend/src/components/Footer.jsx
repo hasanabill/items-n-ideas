1bcf18263d2c31e5d50206fde12c4a29a4110ef0
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-05-19 00:22:38 +0600

    bug

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-gray-300 py-6 mt-32">
+        <footer className="bg-gray-800 text-gray-300 py-6 ">
             <div className="container mx-auto flex justify-center items-center">
                 <p className="text-center">&copy; 2024 Items & Ideas. All rights reserved.</p>
             </div>
