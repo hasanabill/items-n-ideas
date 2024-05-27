ab0e7664b8b3c3b58c119290e8482c63bd073a2d
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-05-27 23:47:19 +0600

    pass

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
                             <label htmlFor="password" className="sr-only">Password</label>
                             <input id="password" name="password" type="password" autoComplete="current-password" {...register("password", { required: true })} className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm" placeholder="Password" />
                         </div>
-                        {errors.password && <p className="mt-1 text-sm text-red-600">This field is required</p>}
+                        {errors.password && <p className="mt-1 text-sm text-red-600">Enter Password</p>}
                     </div>
                     <div>
                         <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
